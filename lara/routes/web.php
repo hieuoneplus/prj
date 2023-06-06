6af4656234f2d2a7199b79e67fc0e8106b10b1e8
hieuoneplus <nshd183@gmail.com>
2023-06-06 17:04:48 +0700

    Add files via upload

---
+++ lara/routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+/*Route::get('/', function () {
+    //return view('welcome');
+    return view('home');
+});
+*/
+
+Route::get('/',function () {
+   
+    $data = [
+        'message' => 'Hello World from Laravel'
+    ];
+    return response()->json($data);
+});
+
+Route::get('/unicode', function () {
+    return view('home');
+});
